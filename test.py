183b609c691dfee87784fd071703242c73f1e41a
Muxczf <fegfrank@gmail.com>
2019-05-20 16:51:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 import Tkinter
 from Tkconstants import *
 import tkMessageBox
+import random
 
 def show_okcancel_msgbox(root, label_text, ok_text, ok_cb, cancel_text, cancel_cb):
 	if label_text:
@@ -29,32 +30,76 @@ def set_main_box_msg(*args):
 	if args[0][2]:
 		main_cancel_text.set(args[0][2])
 
-def show_question(title_msg = "Ask question", text_msg = "xx"):
-	return tkMessageBox.askyesno(title_msg, text_msg)
+def start_game():
+	global score, B_SHOW_TAOXIN
+	
+	n = 0
+	last_answer_res = True
+	while score < SCORE_ENDGAME and n < len(DICT_QUESTION):
+		if DICT_ANSWER[n] == show_question(text_msg = DICT_QUESTION[n]):
+			score += 2
+			show_right("", "%s\n可以可以，有那么点意思 (*^．^*) 親一個!!!\n你现在已经有 %d 分啦" % (get_name(), score))
+			last_answer_res = True
+		else:
+			score -= 2
+			show_wrong("", "%s\n这么简单你都答不对（╯‵□′）╯︵┴─┴\n你现在只有 %d 分了哦" % (get_name(), score))
+			last_answer_res = False
+		n += 1
+	
+	# 结算
+	if score >= SCORE_ENDGAME:
+		show_info("", "行吧~行吧~ 居然全答对了，我颜面荡然无存，小看你了╮(－_－)╭")
+		B_SHOW_TAOXIN = True
+	else:
+		show_info("", "t4")
+		score = 0
 
-def show_right(title_msg = "right", text_msg = "xx"):
+def end_game():
+	global root, B_SHOW_TAOXIN
+
+	show_right("", "t2")
+
+	show_taoxin()
+
+def show_info(title_msg = "", text_msg = "show_info"):
 	return tkMessageBox.showinfo(title_msg, text_msg)
 
-def show_wrong(title_msg = "wrong", text_msg = "xx"):
+def show_question(title_msg = "这是一个很简单的问题", text_msg = "show_question"):
+	return tkMessageBox.askyesno(title_msg, text_msg)
+
+def show_right(title_msg = "", text_msg = "show_right"):
+	return tkMessageBox.showinfo(title_msg, text_msg)
+
+def show_wrong(title_msg = "", text_msg = "show_wrong"):
 	return tkMessageBox.showerror(title_msg, text_msg)
+
+def get_name():
+	return NAME_LIST[random.randint(0, len(NAME_LIST) - 1)]
 
 def show_taoxin():
 	global root, B_SHOW_TAOXIN
 	if B_SHOW_TAOXIN:
-		# xx
-		pass
-	
-	root.destroy()
+		show_right("", "你该不会以为我啥奖励都不给你吧（＾ω＾）")
+		show_right("", "猜猜是啥（＾ω＾）")
+		show_right("", "猜不到？（＾ω＾）")
+
+
+		show_right("", "t4")
 
 if __name__=="__main__":
 	
+	NAME_LIST = [
+		"body1",
+		"body2",
+		"body3",
+	]
+
 	DICT_QUESTION = [
 		"q1",
 		"q2",
 		"q3",
 		"q4",
 		"q5",
-		"q6",
 	]
 
 	DICT_ANSWER = [
@@ -63,7 +108,6 @@ if __name__=="__main__":
 		True,
 		False,
 		True,
-		False,
 	]
 
 	score = 0
@@ -85,41 +129,16 @@ if __name__=="__main__":
 	main_ok_text 		= Tkinter.StringVar()
 	main_cancel_text 	= Tkinter.StringVar()
 
-	def start_game():
-		global score, B_SHOW_TAOXIN
-		
-		n = 0
-		while score < SCORE_ENDGAME and n < len(DICT_QUESTION):
-			if DICT_ANSWER[n] == show_question(text_msg = DICT_QUESTION[n]):
-				score += 2
-				show_right("right", "cur_score %d" % score)
-			else:
-				score -= 2
-				show_wrong("wrong", "cur_score %d" % score)
-			n += 1
-		
-		# 结算
-		if score >= SCORE_ENDGAME:
-			show_right("right", "all good")
-			B_SHOW_TAOXIN = True
-		else:
-			show_wrong("wrong", "try again")
-
-	def end_game():
-		global root, B_SHOW_TAOXIN
-
-		show_right("last sup")
-
-		show_taoxin()
-		# root.quit()
-
-	set_main_box_msg(["什么鬼", "开始", "结束"])
+	set_main_box_msg(["%s\nt1" % get_name(), "假装知道(つ´ω`)つ", "啥玩意，不xiu得(￣﹁￣)"])
 	show_okcancel_msgbox(root, main_label_text, 
 		main_ok_text,
 		start_game, 
 		main_cancel_text, 
 		end_game)
 
-	root.protocol("WM_DELETE_WINDOW", show_taoxin)
+	def quit_all():
+		show_taoxin()
+		root.destroy()
+	root.protocol("WM_DELETE_WINDOW", quit_all)
 
 	root.mainloop()
